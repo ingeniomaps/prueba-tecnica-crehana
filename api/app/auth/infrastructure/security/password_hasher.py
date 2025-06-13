dfb99c6d91ff0a720784ee1445ecbf262dedc864
Manuel Pinzon <malpisa1@gmail.com>
2025-06-13 12:03:12 -0500

    fix: Cambiando bcrypt a argon2

--- api/app/auth/infrastructure/security/password_hasher.py
+++ api/app/auth/infrastructure/security/password_hasher.py
@@ -3,7 +3,7 @@
 from passlib.context import CryptContext
 
 # Funciones de utilidades para contraseñas
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
